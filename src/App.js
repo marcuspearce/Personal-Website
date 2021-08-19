e0f7a95601201510a820621700997faa8c96cf6b
marcuspearce <marcuspearce7@gmail.com>
2021-08-18 21:56:03 -0700

    implemented basic website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import mp from './ucla marcus.JPG';
 import './App.css';
+
+import { Container, Row, Col } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col>
+            <div className="img-container">
+              <img src={mp} alt="mp"></img>
+              <div className="vl"></div>
+            </div>
+          </Col>
+
+          <Col xs={12} sm={8}>
+            <div className="content">
+              <div className="text-container">
+                <div className="greeting-head">
+                  Hi! <span className="wave">👋</span> I'm Marcus. 
+                </div>
+                <div className="greeting-text">
+                  Glad you came! Here's a little about me:
+                </div>
+                <hr></hr>
+                {/* <hr/> */}
+                <div className="about-container">
+                  <ul className="about-text">
+                    <li>🐻&nbsp; Rising senior studying CS at <span className="ucla"><a className="about-link" href="https://www.cs.ucla.edu/" target="_blank">UCLA</a></span></li>
+                    <li>🧑‍💻&nbsp; SDE Intern at <span className="aws"><a className="about-link" href="https://aws.amazon.com/" target="_blank">Amazon Web Services</a></span></li>
+                    <li>📚&nbsp; Project Manager and Consultant at <span className="tbg"><a className="about-link" href="https://www.thebruingroupconsulting.com/" target="_blank">The Bruin Group</a></span></li>
+                    <li>🕺&nbsp; Pre-pandemic Dancer at <span className="koreos"><a className="about-link" href="https://www.youtube.com/watch?v=8iFPm9Nfb3g" target="_blank">Koreos</a></span> and <span className="sama"><a className="about-link" href="https://www.youtube.com/watch?v=DK_MgCA4YZg&list=PLxpuy5i_iKW4BW9i6gukBXXfd_AiirRCz&index=4" target="_blank">Samahang Modern</a></span></li>
+                    <li>🎮&nbsp; Hard-stuck iron Valorant gamer</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="links-container">
+                <div className="links-group">
+                  <a className="link-text" href="https://www.linkedin.com/in/marcusrpearce/">linkedin</a>
+                  <a className="link-text" href="#">resume</a>
+                  <a className="link-text" href="https://github.com/marcuspearce">github</a>
+                </div>
+              </div>
+            </div>
+          </Col>
+          
+        </Row>
+      </Container>
     </div>
   );
 }
